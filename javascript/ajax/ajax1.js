123fcef671642986ee11ff0f8a5bebe0606f9d32
jia_er_ji <41848029+jiaerji@users.noreply.github.com>
2019-01-06 12:52:12 +0800

    Add files via upload

---
+++ javascript/ajax/ajax1.js
@@ -0,0 +1,45 @@
+var ajax = function(url, method = "GET", data = {}, async = true, success, error, resType = "", headers = {}) {
+    //设置变量默认值
+    // method = mehtod || "GET";
+    // async = async || true;//异步执行，后边的脚本不用等到ajax请求到数据之后才执行
+    // data = data || "";
+    // resType = resType || "";
+    //数据校验
+    if(!url || url === '') {
+        throw new Error('url不能为空');//throw用来抛出异常
+    }
+    if(method==="GET" && data != "") {
+        throw new Error('请将get请求参数写在url里');
+    }
+    method = method.toUpperCase();//将小写全部转换为大写
+    if(window.XMLHttpRequest) {
+        var xhr = new XMLHttpRequest();
+    } else {
+        var xhr = new ActiveXObject("Microsft.XMLHTTP");
+    }
+    xhr.open(method, url, async);//XMLHttpRequest.open()初始化 HTTP 请求参数
+    //设置请求头
+    //判断是否设置
+    //循环 headers 设置请求头
+    //xhr.setRequestHeader("Content-type", "application/x-www-form-urlencodded");
+    //设置返回数据格式
+    if(async){//如果设置了同步就不能设置返回数据格式
+        xhr.responseType = resType; //设置返回响应数据的类型，在未设置responseType的时候默认为Text
+    }
+    //send(data) 将请求发送到服务器data仅用于post
+    xhr.send(data);
+    //后面为HTTP响应
+    xhr.onreadystatechange = function() {//onreadystatechange为存储函数，每当readystate值变化的时候都会调用该函数
+        if(xhr.readyState == 4) {
+            var res = xhr.response;//返回的响应
+            if(xhr.status >=200 && xhr.statu <300 || xhr.status == 304) {//状态码为304表示请求的资源并没有被修改，可以直接使用浏览器中缓存的版本
+                success && success(res);
+            } else {
+                error && error(res);
+            }
+        }
+    }
+}
+
+
+ajax("","GET","",true,function(res){console.log(res);},function(error){console.log(error);},'json');
---
+++ javascript/ajax/ajax2.js
@@ -0,0 +1,88 @@
+var ajax = ({ url, method = "GET", data = {}, async = true, success, error, resType = "", headers = {} }) => {//箭头函数相当于 匿名函数， 简化了函数的定义
+    //错误判断 url为必填项 
+    if (!url || url === '') {
+        throw new Error('请求地址不能为空');
+    }
+    //变量声明
+    let dataArr = [];
+    let dataStr = "";
+    let xhr = new XMLHttpRequest();//不兼容低版本浏览器 
+    let formData = new FormData();
+    //将小写转换成大写
+    method = method.toUpperCase();
+
+    //初始化data 
+    switch (method) {
+            
+        case 'POST':
+            for (let key in data) {
+                formData.append(`${key}`, `${headers[key]}`);//将data转换成 FormData 对象的字段 
+            }
+            break;
+        case 'GET':
+            for (let key in data) {
+                dataArr.push(`${encodeURIComponent(key)}=${encodeURIComponent(data[key])}`);
+            }
+            strData = dataArr.join('&');
+            break;
+    }
+    //设置返回数据格式 
+    if (typeof async === 'boolean' && async) {//如果设置了同步就不能设置返回数据格式 
+        if(typeof resType === 'string'){ 
+            xhr.responseType = resType; // 在不设置responseType的时候默认为 text 
+        } else{ throw new Error('设置返回数据格式时，请使用字符串类型'); 
+        } 
+    } //发起请求 
+    switch (method){ 
+        case 'POST': 
+            xhr.open(method , url , async); 
+        break; 
+        case 'GET': 
+            xhr.open(method , `${url}?${strData}` , async); 
+        break; 
+    }
+    //设置请求头 必须在 xhr.open() 后才可以 
+    //判断是否设置 
+    if(typeof headers === 'object' && Object.keys(headers).length !== 0){ 
+        //循环 headers 设置请求头 
+        for(let key in headers){ 
+            xhr.setRequestHeader(`${key}`,`${headers[key]}`);//`${}`字符串占位符拼接字符串
+            // xhr.setRequestHeader("Content-type", "application/x-www-form-urlencoded"); 
+        } 
+        //console.log(Object.keys(headers));//返回一个数组，数组成员是对象中所有的键 
+        //console.log(Object.values(headers));//返回一个数组，数组成员是对象中所有的值 
+    }
+    xhr.send(formData);//发送数据 
+    //监听状态 
+    xhr.onreadystatechange = ()=>{ 
+        if(xhr.readyState === 4){ 
+            let res = xhr.response; 
+            if(xhr.status >= 200 && xhr.status < 300 || xhr.status === 304){ 
+                typeof success === 'function' && success(res); 
+            } else{ typeof error === 'function' && error(res); 
+            } 
+        } 
+    } 
+}
+
+
+ajax({
+    url:'',
+    method:'post',
+    resType:'json',
+    headers:{
+        id:123,
+        name:123,
+        key:123,
+    },
+    data:{
+        name: "123",
+        id: "123"
+    },
+    success(res){
+        console.log(res);
+    },
+    error(){
+        console.log('error')
+    },
+});
